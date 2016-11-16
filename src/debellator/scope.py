ca7a190f463cf067fbb3c54567c1b21ceb5edbe4
Oliver Berger <oliver@digitalarchitekt.de>
2016-11-16 12:49:52 +0100

    tests and scope

--- src/debellator/scope.py
+++ src/debellator/scope.py
@@ -13,26 +13,54 @@ class Scope:
         self.levels = []
 
     def push(self, variables, defaults=None):
-        self.levels.append(dict(defaults, **variables))
+        self.levels.append(dict(defaults or {}, **variables))
 
-    def pop(self, *exports):
+    def pop(self, exports=None):
         """Remove a scope level.
 
-        :param exports: a list of names which should be exported to the next scope level
+        :param exports: a mapping of names and their value branch which should be exported to the next scope level
         """
         if not self.levels:
             raise RuntimeError("The scope has no more levels to pop off: {}".format(self))
 
-        exported = {name: value for name, value in self.levels.pop().items() if name in exports}
+        exported = {name: self[dotted_name] for name, dotted_name in exports.items()}
+
+        variables = self.levels.pop()
+
         level = self.levels[-1] if self.levels else self.root
         level.update(exported)
 
-    def __getitem__(self, name):
-        for variables in reversed((self.root,), self.levels):
+        return variables
+
+    def __iter__(self):
+        """Create a reversed iterator over the stacked levels."""
+        def _gen():
+            yield from reversed(self.levels)
+            yield self.root
+
+        return iter(_gen())
+
+    def __getitem__(self, dotted_name):
+        """Resolve a dotted name to a value."""
+        name, _, dotted_name = dotted_name.partition('.')
+
+        for variables in self:
             try:
-                return variables[name]
+                variable = variables[name]
+                break
 
             except KeyError:
                 pass
+        else:
+            # if we did not found a variable
+            raise KeyError('`{}` is undefined in this scope: {}'.format(name, self))
 
-        raise KeyError('`{}` is undefined in this scope: {}'.format(name, self))
+        # descend into variable to return only a branch
+        while dotted_name:
+            name, _, dotted_name = dotted_name.partition('.')
+            if name in variable:
+                variable = variable[name]
+            else:
+                raise KeyError('`{}` is not defined: {}'.format(name, variable))
+
+        return variable
